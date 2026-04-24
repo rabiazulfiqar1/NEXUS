b90f581b1b635c254eef9fba91663fccd50bb397
del-b2l <b4t001k@gmail.com>
2026-04-24 19:28:43 +0500

    test: get session access token

---
+++ tests/test.py
@@ -0,0 +1,6 @@
+from supabase import create_client
+from app.core.config import SUPABASE_SERVICE_ROLE_KEY, SUPABASE_URL
+
+client = create_client(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
+res = client.auth.sign_in_with_password({"email": "rzulfiqar889@gmail.com", "password": "Rabiazulfiqar123"})
+print("token:\n", res.session.access_token)
